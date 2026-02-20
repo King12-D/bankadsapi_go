7f5bf6c88c96e12b9eb14a61b60ce04e1cb5a87e
King Dav <mickyyoung660@gmail.com>
2026-02-20 20:01:34 +0100

    Ignore 🫩

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
     port := os.Getenv("PORT")
     if port == "" {
-        port = "8080"
+        port = "3000"
     }
 
     http.HandleFunc("/ads/create", handlers.CreateAd)
